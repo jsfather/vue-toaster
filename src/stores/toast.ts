d28610a4225caf4277ec20cbb9dc7f7b3c09239d
jsfather <keyvanmatinrad@outlook.com>
2024-11-22 16:28:47 +0330

    the toast is ready!

--- src/stores/toast.ts
+++ src/stores/toast.ts
@@ -1,12 +1,28 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import type {Toast} from "@/types/toast"
+import {defineStore} from "pinia";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
+export const useToastStore = defineStore('toastStore', {
+    state: () => ({
+        toasts: [{message: 'test', type: "success", duration: 'permanent', dismissible: true}] as Toast[],
+    }),
+    actions: {
+        addToast(toast: Toast) {
+            if (toast.duration === undefined) toast.duration = 5000
+            if (toast.dismissible === undefined) toast.dismissible = true
+            this.toasts.unshift(toast)
+            if (toast.duration !== 'permanent') {
+                const interval = setInterval(() => {
+                    const index = this.toasts.findIndex(item => item === toast);
+                    this.toasts[index].duration = Number(this.toasts[index].duration) - 1000
+                    if (Number(this.toasts[index].duration) <= 0) {
+                        this.deleteToast(index)
+                        clearInterval(interval)
+                    }
+                }, 1000)
+            }
+        },
+        deleteToast(index: number) {
+            this.toasts.splice(index, 1)
+        }
+    }
 })
